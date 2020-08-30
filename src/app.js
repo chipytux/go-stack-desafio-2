12f99efa78399ea5b2f304d44ceeaa27f97318a0
chipytux <chipytux@gmail.com>
2020-08-29 22:58:01 -0300

    Solução do Desafio 2

--- src/app.js
+++ src/app.js
@@ -1,33 +1,59 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+const repositories = {};
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(Object.values(repositories));
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs } = request.body;
+  const id = uuidv4();
+  
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes:0
+  }
+
+  repositories[id]  = repository;
+  response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+const idMiddleware = (request,response,next) => {
+  const {id} = request.params;
+  if(!repositories[id]){
+    return response.sendStatus(400);
+  }
+  next()
+}
+
+app.put("/repositories/:id", idMiddleware, (request, response) => {
+  const {title, url, techs } = request.body;
+  const {id} = request.params;
+  repositories[id] = { ...repositories[id], title, url, techs};
+  return response.json(repositories[id]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", idMiddleware, (request, response) => {
+  const {id} = request.params;
+  delete repositories[id];
+  return response.sendStatus(204)
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", idMiddleware, (request, response) => {
+  const {id} = request.params;
+  repositories[id].likes += 1;
+  return response.json(repositories[id]);
 });
 
 module.exports = app;
